1e3dbc6f350d9ce43c9f395a6155f43f13566cff
nareshh49 <124552811+nareshh49@users.noreply.github.com>
2023-07-22 13:11:15 +0530

    java Program

---
+++ hello.java
@@ -0,0 +1,16 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'javac hello.java'
+            }
+        }
+        stage('Run') {
+            steps {
+                sh 'java hello'
+            }
+        }
+    }
+}
